96cdcee5f3324bf8f3c51a75d49071aa39763d72
djulien <djulien17@thejuliens.net>
2018-06-18 15:57:38 -0700

    code

--- EscapePhone.c
+++ EscapePhone.c
@@ -21,7 +21,7 @@
 #define SERIAL_DEBUG
 #define TIMER1_DEBUG
 #define CLOCK_DEBUG
-#define LED_DEBUG
+#define SCAN_DEBUG
 
 
 //#include <xc.h> //automatically selects correct device header defs
@@ -36,6 +36,25 @@
 //#include "serial.h"
 //#include "zc.h"
 //#include "outports.h"
+
+
+//TODO: move to helpers:
+//encode port A and port B/C into a 16-bit value to allow subsequent split using generic port logic:
+//port A in upper byte, port B/C in lower byte
+#define PORTAPIN(portpin)  IIFNZ(isPORTA(portpin), PINOF(portpin))
+//#define PORTBPIN(portpin)  IIFNZ(isPORTB(portpin), PINOF(portpin))
+//#define PORTCPIN(portpin)  IIFNZ(isPORTC(portpin), PINOF(portpin))
+#define PORTBCPIN(portpin)  IIFNZ(isPORTBC(portpin), PINOF(portpin))
+
+#define PORTAMASK(portpin)  IIFNZ(isPORTA(portpin), 1 << PINOF(portpin))
+//#define PORTBMASK(portpin)  IIFNZ(isPORTB(portpin), 1 << PINOF(portpin))
+//#define PORTCMASK(portpin)  IIFNZ(isPORTC(portpin), 1 << PINOF(portpin))
+#define PORTBCMASK(portpin)  IIFNZ(isPORTBC(portpin), 1 << PINOF(portpin))
+//#define pin2bits16(pin)  ABC2bits(IIFNZ(isPORTA(pin), 1 << PINOF(pin)), IIFNZ(isPORTBC(pin), 1 << PINOF(pin)))
+#define PORTMAP16(portpin)  ((PORTAMASK(portpin) << 8) | PORTBCMASK(portpin))
+//#define ABC2bits16(Abits, BCbits)  ((Abits) << 8) | ((Bbits) & 0xff))
+#define Abits(bits16)  ((bits16) >> 8)
+#define BCbits(bits16)  ((bits16) & 0xff)
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -59,11 +78,189 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 ////
+/// Keyboard scanning:
+//
+
+//cols (3) on Port A, rows (4) on Port B/C:
+//#define LED_PIN  RC2
+//#define _LED_PIN  0xC2
+//#define _LED_MASK  PORTMAP16(_LED_PIN)
+#define COL6_PIN  RA5
+#define COL7_PIN  RA4
+#define COL8_PIN  RA2
+
+#define ROW0_PIN  RC0
+#define ROW1_PIN  RC1
+#define ROW2_PIN  RC2
+#define ROW3_PIN  RC3
+
+#define _COL6_PIN  0xA5
+#define _COL7_PIN  0xA4
+#define _COL8_PIN  0xA2
+
+#define _ROW0_PIN  0xC0
+#define _ROW1_PIN  0xC1
+#define _ROW2_PIN  0xC2
+#define _ROW3_PIN  0xC3
+
+#define COL6_MASK  PORTMAP16(_COL6_PIN)
+#define COL7_MASK  PORTMAP16(_COL7_PIN)
+#define COL8_MASK  PORTMAP16(_COL8_PIN)
+
+#define ROW0_MASK  PORTMAP16(_ROW0_PIN)
+#define ROW1_MASK  PORTMAP16(_ROW1_PIN)
+#define ROW2_MASK  PORTMAP16(_ROW2_PIN)
+#define ROW3_MASK  PORTMAP16(_ROW3_PIN)
+
+//rows = output, cols = input (allows slightly more efficient scanning):
+#define COLPORT  PORTA
+#define ROWPORT  PORTBC
+#define COLbits(bits16)  Abits(bits16)
+#define ROWbits(bits16)  BCbits(bits16)
+
+//combine for generic port logic:
+//#define COL_PINS  ((PIN2BIT(RA5) | PIN2BIT(RA4) | PIN2BIT(RA2)) << 8)
+//#define ROW_PINS  (PIN2BIT(RC0) | PIN2BIT(RC0) | PIN2BIT(RC0) | PIN2BIT(RC0))
+#define COLSIN_MASK  (COL6_MASK | COL7_MASK | COL8_MASK)
+#define ROWSOUT_MASK  (ROW0_MASK | ROW1_MASK | ROW2_MASK | ROW3_MASK)
+
+//#define COLROW(c, r)  ABC2bits(r, c)
+//#define COL_PINS  (pin2bits16(COL6) | pin2bits16(COL7) | pin2bits16(COL8))
+//#define ROW_PINS  (pin2bits16(ROW0) | pin2bits16(ROW1) | pin2bits16(ROW2) | pin2bits16(ROW3))
+
+//#define KEY_1  COLROW(COL6, ROW0)
+//#define KEY_2  COLROW(COL7, ROW0)
+//#define KEY_3  COLROW(COL8, ROW0)
+//#define KEY_4  COLROW(COL6, ROW1)
+//#define KEY_5  COLROW(COL7, ROW1)
+//#define KEY_6  COLROW(COL8, ROW1)
+//#define KEY_7  COLROW(COL6, ROW2)
+//#define KEY_8  COLROW(COL7, ROW2)
+//#define KEY_9  COLROW(COL8, ROW2)
+//#define KEY_STAR  COLROW(COL6, ROW3)
+//#define KEY_0  COLROW(COL7, ROW3)
+//#define KEY_HASH  COLROW(COL8, ROW3)
+
+
+#ifdef SCAN_DEBUG //debug
+ #ifndef debug
+  #define debug() //define debug chain
+ #endif
+//define globals to shorten symbol names (local vars use function name as prefix):
+    AT_NONBANKED(0) volatile uint16_t scan_debug_val1;
+    AT_NONBANKED(0) volatile uint16_t scan_debug_val2;
+    AT_NONBANKED(0) volatile uint16_t scan_debug_val3;
+    AT_NONBANKED(0) volatile uint16_t scan_debug_val4;
+    AT_NONBANKED(0) volatile uint16_t scan_debug_val5;
+    AT_NONBANKED(0) volatile uint16_t scan_debug_val6;
+//    AT_NONBANKED(0) volatile uint16_t zcpin_debug; //= _ZC_PIN;
+ INLINE void scan_debug(void)
+ {
+    debug(); //incl prev debug info
+//use 16 bits to show port + pin:
+    scan_debug_val1 = COL6_MASK
+    scan_debug_val2 = ROW3_MASK;
+    scan_debug_val3 = Abits(ROWSOUT_MASK);
+    scan_debug_val4 = BCbits(ROWSOUT_MASK);
+    scan_debug_val5 = ROWbits(COLSINPORTAMASK(_LED_PIN);
+    scan_debug_val6 = PORTBCMASK(_LED_PIN);
+//    serin_debug = _SERIN_PIN;
+//    serout_debug = _SEROUT_PIN;
+//    zcpin_debug = _ZC_PIN;
+ }
+ #undef debug
+ #define debug() scan_debug()
+#endif
+
+
+//;initialize keyboard + serial port pins:
+//grouped to reduce bank selects
+INLINE void port_init(void)
+{
+	init(); //prev init first; NOTE: no race condition with cooperative event handling (no interrupts)
+//enable digital I/O, disable analog functions:
+	IFANSELA(ANSELA = 0); //must turn off analog functions for digital I/O
+	IFANSELBC(ANSELBC = 0);
+//weak pull-ups for input pins are not floating:
+    WPUA = 0xff & ~0;
+    IFWPUBC(WPUBC = 0xff & ~0);
+//set output pins, leave non-output pins as hi-Z:
+    TRISA = 0xff & ~(Abits(ROWSOUT_MASK) | Abits(SEROUT_MASK)); //0b111111; //Abits(COL_PINS) | Abits(pin2bits16(SERIN);
+    TRISBC = 0xff & ~(BCbits(ROWSOUT_MASK) | BCbits(SEROUT_MASK)); //0b010000; //BCbits(COL_PINS) | BCbits(pin2bits16(SERIN);
+    PORTA = PORTBC = 0;
+    IFCM1CON0(CM1CON0 = ~0); //;configure comparator inputs as digital I/O (no comparators); overrides TRISC (page 63, 44, 122); must be OFF for digital I/O to work! needed for PIC16F688; redundant with POR value for PIC16F182X
+//	TRISA = ~PORTA_BITS & TRISA_INIT;
+//	TRISBC = PORTBC_BITS & TRISBC_INIT;
+//	PORTA = 0;
+//	PORTBC = 0;
+}
+#undef init
+#define init()  led_init() //function chain in lieu of static init
+
+
+//check for key pressed:
+//ZERO = key-pressed flag, WREG = key#
+void keypress_WREG()
+{
+//    CARRY = TRUE;
+    ZERO = FALSE;
+//    for (;;) //scan until key received
+//    {
+//    PORTA = Abits(ROW0);
+        ROWPORT = ROWbits(ROW0_MASK);
+        if (COL6_PIN) RETLW(1);
+        if (COL7_PIN) RETLW(2);
+        if (COL8_PIN) RETLW(3);
+
+        ROWPORT = ROWbits(ROW1_MASK);
+        if (COL6_PIN) RETLW(4);
+        if (COL7_PIN) RETLW(5);
+        if (COL8_PIN) RETLW(6);
+
+        ROWPORT = ROWbits(ROW2_MASK);
+        if (COL6_PIN) RETLW(7);
+        if (COL7_PIN) RETLW(8);
+        if (COL8_PIN) RETLW(9);
+
+        ROWPORT = ROWbits(ROW3_MASK);
+        if (COL6_PIN) RETLW(11);
+        if (COL7_PIN) RETLW(10); //avoid confusion with null/0
+        if (COL8_PIN) RETLW(12);
+//    }
+//    CARRY = FALSE;
+//    RETLW(0);
+    ZERO = TRUE;
+}
+
+
+INLINE void scankey_50msec(void)
+{
+	on_tmr_50msec(); //prev event handlers first
+}
+#undef on_tmr_50msec
+#define on_tmr_50msec()  scankey_50msec() //event handler function chain
+
+
+//inline void on_tmr1_debounce()
+//{
+//    on_tmr1();
+//}
+
+//bkg event handler:
+//void yield()
+//{
+//    on_tmr1(); //only need Timer 1 for debounce
+//}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////
 /// led (test)
 //
 
 #define LED_PIN  RC2
 #define _LED_PIN  0xC2
+#define _LED_MASK  PORTMAP16(_LED_PIN)
 
 #if 0
 #define _LED_PIN  PORTPIN(_PORTA, bit2inx(_RA5))
@@ -105,18 +302,6 @@ volatile BANK0 uint8_t portbuf[2];
 #endif
 
 
-#define PORTAPIN(portpin)  IIFNZ(isPORTA(portpin), PINOF(portpin))
-#define PORTBPIN(portpin)  IIFNZ(isPORTB(portpin), PINOF(portpin))
-#define PORTCPIN(portpin)  IIFNZ(isPORTC(portpin), PINOF(portpin))
-#define PORTBCPIN(portpin)  IIFNZ(isPORTBC(portpin), PINOF(portpin))
-
-#define PORTAMASK(portpin)  IIFNZ(isPORTA(portpin), 1 << PINOF(portpin))
-#define PORTBMASK(portpin)  IIFNZ(isPORTB(portpin), 1 << PINOF(portpin))
-#define PORTCMASK(portpin)  IIFNZ(isPORTC(portpin), 1 << PINOF(portpin))
-#define PORTBCMASK(portpin)  IIFNZ(isPORTBC(portpin), 1 << PINOF(portpin))
-#define PORTMAP16(portpin)  ((PORTAMASK(portpin) << 8) | PORTBCMASK(portpin))
-
-#define _LED_MASK  PORTMAP16(_LED_PIN)
 //encode port A and port B/C into one 16-bit value:
 //port A in upper byte, port B/C in lower byte
 //#define ABC2bits16(Abits, BCbits)  ((Abits) << 8) | ((Bbits) & 0xff))
